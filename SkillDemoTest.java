90601aef61d9b5455c63aa520753297dbbcdae26
stevo0718 <jiw084@ucsd.edu>
2022-04-27 17:56:08 -0700

    Change

---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(3, SkillDemo.add(4,1));
+    }
+}
+
